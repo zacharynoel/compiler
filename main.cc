e1d3a66c82f2f51037688deeea6e935be0f99edf
Zak Noel <zaknoel@gmail.com>
2016-05-03 18:30:06 -0700

    print/error checks

--- main.cc
+++ main.cc
@@ -10,10 +10,16 @@
 using namespace std;
 
 vector<string> reg = {"A","B","C","I","J","X","Y","Z"};
+vector<string> print_stuff;
+vector<int> valid_lines;
+vector<int> go_to_lines;
+int word_counter = 0;
 
 int valid (string command){
 	int counter = 4;
 	for(string val : reg){
+		//if(counter == 7)
+		//	counter++;
 		if(val == command){
 			return counter;
 		}
@@ -41,6 +47,12 @@ int main(int argc, char **argv) {
 	}
 	outs << ".global main\nmain:\n"; //Get the file ready to rock
 	outs << "\tPUSH {LR}\n\tPUSH {R4-R12}\n\n";
+	for(int i = 4; i <= 11; i++){
+		//if(i == 7)
+		//	i++;
+		outs << "\tMOV R" << i << ", #0" << endl;
+	}
+	outs << endl;
 
 	int line_no = 0;
 	while (cin) {
@@ -49,28 +61,31 @@ int main(int argc, char **argv) {
 		line_no++;
 		if (!cin) break;
 		transform(s.begin(), s.end(), s.begin(), ::toupper); //Uppercaseify
-		auto it = s.find("END"); //TERMINATE COMPILER
+		auto it = s.find("QUIT"); //TERMINATE COMPILER
 		if (it != string::npos) break;
 		stringstream ss(s); //Turn s into a stringstream
 		int label;
 		ss >> label;
 		if (!ss) die(line_no);
+		valid_lines.push_back(label);
 		outs << "line_" << label << ":\n"; //Write each line number to the file ("line_20:")
 		string command;
 		ss >> command;
 		if (!ss) die(line_no);
 
 		if (command == "REM") {
+			valid_lines.pop_back();
 			continue;
 		}
 		else if (command == "GOTO") {
 			int target;
 			ss >> target;
 			if (!ss) die(line_no);
+			go_to_lines.push_back(target);
 			outs << "\tBAL line_" << target << endl;
 			continue;
 		}
-		else if (command == "EXIT") {
+		else if (command == "EXIT" || command == "END") {
 			outs << "\tBAL quit\n";
 			continue;
 		}
@@ -84,13 +99,12 @@ int main(int argc, char **argv) {
 				if(command == "="){
 					ss >> command;
 					int reg_num2 = valid(command);
-					//First check if it's an immediate value
-					if(reg_num2 == -1){
+					ss >> command;
+					if(reg_num2 == -1 && !ss){
 						outs << "\tMOV R" << reg_num << ", #" << command << endl;
 						continue;
 					}
-					ss >> command;
-					if(!ss){
+					else if(reg_num2 != -1 && !ss){
 						outs << "\tMOV R" << reg_num << ", R" << reg_num2 << endl;
 					}
 					else{
@@ -104,19 +118,54 @@ int main(int argc, char **argv) {
 								outs << "\tSUB ";
 							else if(sign == "*")
 								outs << "\tMUL ";
+							else
+								die(line_no);
 							outs << "R" << reg_num << ", R" << reg_num2 << ", R" << reg_num3 << endl;
 						}
+						else die(line_no);
 					}
 				}
+				else die(line_no);
 			}
+			else die(line_no);
 		}
 
 		else if(command == "PRINT"){
 			ss >> command;
 			int reg_num = valid(command);
 			if(reg_num != -1){
-				outs << "\tMOV R1, R" << reg_num << endl;
+				ss >> command;
+				if(ss)
+					die(line_no);
+				outs << "\tMOV R0, R" << reg_num << endl;
 				outs << "\tBL print_number" << endl;
+			}
+			else{
+				string temp;
+				while(ss){
+					temp += command;
+					ss >> command;
+					if(ss)
+						temp += " ";
+				}
+				if(command[0] != '\"' && command[command.size()-1] != '\"')
+					die(line_no);
+				word_counter++;
+				command = temp;
+				command.erase(remove(command.begin(), command.end(), '"'), command.end());
+				string full_str = "word" + to_string(word_counter) + ": .ascii \"" + command + "\"";
+				print_stuff.push_back(full_str);
+				
+				outs << "\tMOV R0, #1" << endl;
+				outs << "\tLDR R1, =word" << to_string(word_counter) << endl;
+				outs << "\tMOV R2, #" << command.size() << endl;
+				outs << "\tMOV R12, R7" << endl;
+				outs << "\tMOV R7, #4" << endl;
+				outs << "\tSWI 0" << endl;
+				outs << "\tMOV R7, R12" << endl;
+
+				//outs << "\tLDR R0, =word" << to_string(word_counter) << endl;
+				//outs << "\tBL print_string" << endl;
 			}
 		}
 
@@ -129,7 +178,7 @@ int main(int argc, char **argv) {
 			
 			ss >> command;
 			int register2 = valid(command);
-			
+
 			ss >> command;
 			string then = command;
 			
@@ -138,6 +187,9 @@ int main(int argc, char **argv) {
 			
 			ss >> command;
 			string lineNum = command;
+			int templine;
+			stringstream(lineNum) >> templine;
+			go_to_lines.push_back(templine);
 
 			if (register1 != -1 && register2 != -1) {
 				outs << "\tCMP R" << register1 << ", R" << register2 << "\n";
@@ -160,18 +212,58 @@ int main(int argc, char **argv) {
 					else if (comparison == "!="){
 						outs << "\tBNE line_" << lineNum << "\n";
 					}
+					else
+						die(line_no);
 				}
+				else die(line_no);
+
+				ss >> command;
+				if(!ss)
+					continue;
+				string else_check = command;
+				ss >> command;
+				go_to = command;
+				if(else_check == "ELSE" && go_to == "GOTO"){
+					ss >> command;
+					lineNum = command;
+					stringstream(lineNum) >> templine;
+					go_to_lines.push_back(templine);
+					outs << "\tB line_" << lineNum << "\n";
+				}
+				else die(line_no);
 			}
+			else die(line_no);
 		}
+		
+		else if(command == "INPUT"){
+		}
+		
+		else die(line_no);
+	}
+
+	bool check;
+	int count = 0;
+	for(int val : go_to_lines){
+		check = false;
+		for(int val2 : valid_lines){
+			if(val == val2)
+				check = true;
+		}
+		if(!check)
+			die(valid_lines.at(count));
+		count++;
 	}
 
 	//Clean up the file at the bottom
 	outs << "\nquit:\n\tMOV R0, #42\n\tPOP {R4-R12}\n\tPOP {PC}\n"; //Finish the code and return
+	outs << "\n.data" << endl;
+	for(string word : print_stuff)
+		outs << "\t" << word << endl;
 	outs.close(); //Close the file
 
 	if (assemble_only) return 0; //When you're debugging you should run bb8 with a parameter
 
-	if (system("g++ main.s")) { //Compile your assembler code and check for errors
+	if (system("g++ main.s print.o")) { //Compile your assembler code and check for errors
 		cout << "Assembling failed, which means your compiler screwed up.\n";
 		return 1;
 	}
--- main.s
+++ main.s
@@ -3,13 +3,32 @@ main:
 	PUSH {LR}
 	PUSH {R4-R12}
 
-line_10:
-	MOV R9, #5
-line_20:
-	MOV R1, R9
-	BL print_number
+	MOV R4, #0
+	MOV R5, #0
+	MOV R6, #0
+	MOV R7, #0
+	MOV R8, #0
+	MOV R9, #0
+	MOV R10, #0
+	MOV R11, #0
 
-quit:
-	MOV R0, #42
-	POP {R4-R12}
-	POP {PC}
+line_1:
+line_2:
+line_3:
+line_10:
+	CMP R9, R10
+	BNE line_30
+	B line_40
+line_30:
+line_40:
+line_50:
+	MOV R0, R9
+	BL print_number
+line_60:
+	MOV R0, #1
+	LDR R1, =word1
+	MOV R2, #1
+	MOV R12, R7
+	MOV R7, #4
+	SWI 0
+	MOV R7, R12
